f9817f5391a2a1718de335d867723df1a54ce33b
Valerio Adragna <valerioadragna89@hotmail.it>
2020-05-25 13:05:58 +0200

    getting the set up ready

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("test", $);
+(function () {
+  let column24 = $(".column.twentyfour");
+  let column19 = $(".column.nineteen");
+  let column6 = $(".column.six");
+  let column1 = $(".column.one").children();
+
+  console.log(column1, column19.children());
+
+  column24.children().eq(0).addClass("red");
+  column24.children().eq(1).addClass("red");
+
+  for (let i = 0; i <= 4; i++) {
+    column19.children().eq(i).addClass("white");
+    column6.children().eq(i).addClass("red");
+  }
+})();
